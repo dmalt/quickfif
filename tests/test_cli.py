98806a4007b80c34c5f8c2fd2f85f15b8e011410
Dmitrii Altukhov <dm.altukhov@ya.ru>
2023-06-23 15:37:06 +0200

    add cli tests; tests for bad argument

---
+++ tests/test_cli.py
@@ -0,0 +1,22 @@
+"""Tests for the CLI."""
+import pytest
+from click.testing import CliRunner
+
+from mne_cli_tools import main
+
+
+@pytest.fixture
+def cli_runner():
+    """Mock interacting with CLI from the console."""
+    return CliRunner()
+
+
+def test_cli_fails_wo_args(cli_runner: CliRunner) -> None:
+    """CLI must fail when executed without any arguments."""
+    assert cli_runner.invoke(main.main).exit_code == 2
+
+
+def test_cli_fails_for_nonexistent_fname(cli_runner: CliRunner) -> None:
+    """CLI must fail when supplied a nonexistent filename."""
+    with cli_runner.isolated_filesystem():
+        assert cli_runner.invoke(main.main, ["Test"]).exit_code == 2
