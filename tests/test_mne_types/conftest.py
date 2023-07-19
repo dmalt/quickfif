12408f5e6d6243e20eccad898c0abb2aa7ed4c84
Dmitrii Altukhov <dm.altukhov@ya.ru>
2023-07-19 22:14:18 +0200

    refactor: split test_raw_fif.py

--- tests/test_mne_types/conftest.py
+++ tests/test_mne_types/conftest.py
@@ -1,8 +1,15 @@
+"""Fixtures for mne_types package testing."""
+from pathlib import Path
 from typing import Callable
 
 import mne
 import numpy as np
+import numpy.typing as npt  # noqa: WPS301 (dotted imports)
 import pytest
+
+from mne_cli_tools.mne_types import raw_fif
+
+FloatArray = npt.NDArray[np.float_]
 
 
 @pytest.fixture
@@ -22,3 +29,37 @@ def create_fake_annots() -> Callable[[float, float], mne.Annotations]:
         return mne.Annotations(onsets, duration=duration, description=description)
 
     return wrapped
+
+
+@pytest.fixture
+def timeseries() -> tuple[FloatArray, float]:
+    """Sample timeseries data with sampling frequency."""
+    sfreq, f1, f2 = 100, 7, 5
+    times = np.linspace(0, 1, int(sfreq), endpoint=False)
+    sim_data = np.array(
+        [
+            np.sin(f1 * 2 * np.pi * times),
+            np.cos(f2 * 2 * np.pi * times),
+        ]
+    )
+    return sim_data, sfreq
+
+
+@pytest.fixture
+def raw_obj(timeseries: tuple[FloatArray, float]) -> mne.io.RawArray:
+    """Sample `mne.io.Raw` object."""
+    sim_data, sfreq = timeseries
+    n_ch = len(sim_data)
+    mne_info = mne.create_info(ch_names=n_ch, ch_types="misc", sfreq=sfreq)
+    return mne.io.RawArray(sim_data, mne_info)
+
+
+@pytest.fixture(params=raw_fif.EXTENSIONS)
+def saved_raw_fif(
+    request: pytest.FixtureRequest, tmp_path: Path, raw_obj: mne.io.Raw
+) -> raw_fif.RawFif:
+    """`RawFif` object with fpath pointing to to saved wrapped `mne.io.Raw` object."""
+    ext = request.param
+    fpath = tmp_path / f"test{ext}"
+    raw_obj.save(fpath)
+    return raw_fif.RawFif(fpath, raw_obj)
--- tests/test_mne_types/test_raw_fif.py
+++ tests/test_mne_types/test_raw_fif.py
@@ -3,49 +3,9 @@ from pathlib import Path
 from typing import Callable
 
 import mne
-import numpy as np
-import numpy.typing as npt  # noqa: WPS301 (dotted imports)
 import pytest
-from numpy.testing import assert_array_almost_equal
-from returns.unsafe import unsafe_perform_io
 
 from mne_cli_tools.mne_types import raw_fif
-
-FloatArray = npt.NDArray[np.float_]
-
-
-@pytest.fixture
-def timeseries() -> tuple[FloatArray, float]:
-    """Sample timeseries data with sampling frequency."""
-    sfreq, f1, f2 = 100, 7, 5
-    times = np.linspace(0, 1, int(sfreq), endpoint=False)
-    sim_data = np.array(
-        [
-            np.sin(f1 * 2 * np.pi * times),
-            np.cos(f2 * 2 * np.pi * times),
-        ]
-    )
-    return sim_data, sfreq
-
-
-@pytest.fixture
-def raw_obj(timeseries: tuple[FloatArray, float]) -> mne.io.RawArray:
-    """Sample `mne.io.Raw` object."""
-    sim_data, sfreq = timeseries
-    n_ch = len(sim_data)
-    mne_info = mne.create_info(ch_names=n_ch, ch_types="misc", sfreq=sfreq)
-    return mne.io.RawArray(sim_data, mne_info)
-
-
-@pytest.fixture(params=raw_fif.EXTENSIONS)
-def saved_raw_fif(
-    request: pytest.FixtureRequest, tmp_path: Path, raw_obj: mne.io.Raw
-) -> raw_fif.RawFif:
-    """`RawFif` object with fpath pointing to to saved wrapped `mne.io.Raw` object."""
-    ext = request.param
-    fpath = tmp_path / f"test{ext}"
-    raw_obj.save(fpath)
-    return raw_fif.RawFif(fpath, raw_obj)
 
 
 @pytest.fixture(params=[True, False], ids=["with annots", "without annots"])
@@ -59,13 +19,6 @@ def maybe_annotated_raw_fif(
         t_start, t_stop = raw_obj.times[0], raw_obj.times[-1]
         raw_obj.set_annotations(create_fake_annots(t_start, t_stop))
     return raw_fif.RawFif(fpath=Path("test_path_raw.fif"), raw=raw_obj)
-
-
-def test_read_works_with_specified_extensions(saved_raw_fif: raw_fif.RawFif) -> None:
-    """Check if raw objects saved with supported extensions are loaded fine."""
-    loaded_raw = unsafe_perform_io(raw_fif.read(saved_raw_fif.fpath).unwrap())
-
-    assert_array_almost_equal(saved_raw_fif.raw.get_data(), loaded_raw.raw.get_data())
 
 
 def test_summary_string_contents(maybe_annotated_raw_fif: raw_fif.RawFif) -> None:
@@ -87,3 +40,13 @@ def test_summary_string_contents(maybe_annotated_raw_fif: raw_fif.RawFif) -> Non
         assert raw_fif.ANNOTS_SECTION_HEADER in summary
     else:
         assert raw_fif.NO_ANNOTS_MSG in summary
+
+
+def test_to_dict_wraps_fpath_and_raw(raw_obj: mne.io.Raw):
+    """Test to_dict wraps fpath and raw."""
+    raw_fif_obj = raw_fif.RawFif(Path("test_fpath_raw.fif"), raw_obj)
+
+    res = raw_fif_obj.to_dict()
+
+    assert raw_fif_obj.fpath in res.values()
+    assert raw_fif_obj.raw in res.values()
